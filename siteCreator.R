c6ad47a09517d4b9f02fa800c97d9ccb64aa394e
Sylphild <pacaicedo@gmail.com>
2023-03-30 20:16:09 -0500

    WebSite: 1%

--- siteCreator.R
+++
@@ -1,7 +0,0 @@
-setwd("/Users/pacaicedo/Ars Programandi et Algorithms/pacaicedo.github.io")
-
-library("blogdown")
-
-install_theme(theme="curtiscde/hugo-theme-massively")
-new_site(theme="curtiscde/hugo-theme-massively")
-build_site()
